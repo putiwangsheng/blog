c72bdb760734dcec757db9f2911b9a58fb8ac846
长凄 <2026929156@qq.com>
2016-03-12 11:10:20 +0800

    renew config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // please set serve-favicon in ./node/app/public path
 
 module.exports = {
-    blogRootPath: "F:\\github\\blog\\Article",
-    mongoUrl: 'mongodb://127.0.0.1:27017/blog',
-    serverPort: '5000',
-    renewInterval: 60, // x seconds
+    blogRootPath: "/home/simplyy/node-server/apps/changqi-blog/Article",
+    mongoUrl: 'mongodb://simplyy:1994522yu@127.0.0.1:27016/blog',
+    serverPort: '8001',
+    renewInterval: 24*3600, // x seconds
     tokenExpireTime: 10 // x hours
 };
